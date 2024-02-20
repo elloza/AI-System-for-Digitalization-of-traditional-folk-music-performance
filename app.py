7478208b689cfe4e9f6c1c9a11752bf599a63369
Loza Ubuntu <loza@usal.es>
2024-02-20 21:07:13 +0100

    Transcription done

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+import datetime
 import streamlit as st
+import json
 from streamlit_player import st_player
 # import util functions
-from utils import convert_vimeo_url_to_embed, convert_youtube_url_to_embed
-
-
+from utils import convert_vimeo_url_to_embed, convert_youtube_url_to_embed, download_audio_of_video, split_audio, transcribe_vocals
 
 # Application title
 st.title('AI tool for digitalization of traditional folk music performances')
@@ -45,30 +45,53 @@ if st.session_state.get('video_loaded', False):
         # Process the video
 
         # Display loading message while processing the video
-        with st.spinner('Procesando video...'):
-            # Simulate the processing of the video with a sleep
-            import time
-            time.sleep(5)
+        with st.spinner('Processing video...'):
 
-        # 1 Download the video to a local folder and get the path
-        # TODO function to download the video from the url and return the path
+            # 1 Download the audio of the video to a local folder and get the path
+            # TODO function to download the audio video from the url and return the path
 
-        # 2 Extract the audio from the video and get the path
-        # TODO function to extract the audio from the video and return the path
+            # generate the name of the file with current date and time dd-mm-yyyy-hh-mm-ss
+            filename = 'audio'
+            print(f'New filename for the audio: {filename}')
 
-        # 3 Split the audio into vocals and accompaniment and get the paths
-        # TODO function to split the audio into vocals and accompaniment and return the paths
+            audio_filepath = download_audio_of_video(url=video_url, audio_name=filename)
+            print(f'Downloaded video Filepath: {audio_filepath}')
 
-        # 4 Transcribe the vocals and get the lyrics
-        # TODO function to transcribe the vocals and return the lyrics
+            # Save video path and metadata in the session state 
+            st.session_state['video_audio_processed_filepath'] = audio_filepath
 
-        # 5 Generate the music score from the accompaniment, return path to score, path to midi and path to pdf
-        # TODO function to generate the music score from the accompaniment
-        # Check this for MIDI player https://github.com/andfanilo/streamlit-midi-to-wav/blob/main/app.py
+            # Time sleep
 
-        # 6 Show the lyrics and the music score
-        # TODO show the lyrics and the music score
-        st.success('Video procesado con éxito.')
+            # 2 Extract the audio from the video and get the path
+            # TODO function to extract the audio from the video and return the path
+            
+            audio_vocals_path, audio_accompaniment_path = split_audio(audio_filepath)
+            print(f'Vocals audio Filepath: {audio_vocals_path}')
+            print(f'Accompaniment audio Filepath: {audio_accompaniment_path}')
+
+            st.session_state['video_audio_vocals_filepath'] = audio_vocals_path
+            st.session_state['video_audio_accompaniment_filepath'] = audio_accompaniment_path
+            
+
+            # 4 Transcribe the vocals and get the lyrics
+            # pip install faster_whisper
+            # TODO function to transcribe the vocals and return the lyrics
+            # Check this for lyrics
+            segments, info = transcribe_vocals(audio_vocals_path)
+
+            print(f'infor: {info}')
+
+            st.session_state['video_lyrics'] = json.dumps(segments)
+            
+            
+
+            # 5 Generate the music score from the accompaniment, return path to score, path to midi and path to pdf
+            # TODO function to generate the music score from the accompaniment
+            # Check this for MIDI player https://github.com/andfanilo/streamlit-midi-to-wav/blob/main/app.py
+
+            # 6 Show the lyrics and the music score
+            # TODO show the lyrics and the music score
+            st.success('Video procesado con éxito.')
 
         # Create a variable in the session to store the state
         if 'video_processed' not in st.session_state:
@@ -80,13 +103,39 @@ if st.session_state.get('video_loaded', False):
 # Information about the video processed
 if st.session_state.get('video_processed', False):
 
-    st.subheader('Lyrics and Music Score')
+    st.subheader('Downloaded audio')
+    # Get the video filepath from the session state
+    filepath = st.session_state['video_audio_processed_filepath']
+    audio_file = open(filepath, 'rb')
+    audio_bytes = audio_file.read()
+    st.audio(audio_bytes, format='audio/wav', start_time=0)
+
+    # Vocals audio
+    st.subheader('Vocals audio')
+    # Get the video filepath from the session state
+    filepath = st.session_state['video_audio_vocals_filepath']
+    audio_file = open(filepath, 'rb')
+    audio_bytes = audio_file.read()
+    st.audio(audio_bytes, format='audio/wav', start_time=0)
+
+    # Accompaniment audio
+    st.subheader('Accompaniment audio')
+    # Get the video filepath from the session state
+    filepath = st.session_state['video_audio_accompaniment_filepath']
+    audio_file = open(filepath, 'rb')
+    audio_bytes = audio_file.read()
+    st.audio(audio_bytes, format='audio/wav', start_time=0)
 
     # Section of lyrics
     st.subheader('Lyrics')
 
     # Text area to show the lyrics
     lyrics = st.text_area('Lyrics:', height=200)
+
+    # Get the lyrics from the session state
+    lyrics_text = st.session_state['video_lyrics']
+    # Set to the text area previously defined
+    lyrics = lyrics_text
 
     # Section for the score PDF with a pdf viewer
     st.subheader('Music Score')
--- utils.py
+++ utils.py
@@ -1,5 +1,167 @@
 ## File class with utility functions for the app
 import os
+import subprocess
+import shlex
+import tempfile
+import json
+import pathlib
+from faster_whisper import WhisperModel
+
+
+# Youtube-dl template
+# CHANGE THIS TO USE youtube-dl in unix
+_RETRIEVE_AUDIO_CMD_TEMPLATE = '''youtube-dl -k -f "bestaudio/best" -ciw -x --extractor-retries 4 -v --audio-quality 0 --audio-format wav -o "{audio_name}.wav" {url}'''
+# Spleeter deezer template
+_SPLIT_AUDIO_CMD_TEMPLATE = '''spleeter separate -p spleeter:2stems -o output {audio_file_path}'''
+
+# Download video from url using youtube-dl
+#python -m pip install --no-cache-dir git+https://github.com/yt-dlp/yt-dlp.git@2023.07.06
+#ln -s $(which yt-dlp) /usr/local/bin/youtube-dl
+# apt-get install -y software-properties-common && sudo apt-get update && apt-get install -y ffmpeg
+
+def download_audio_of_video(url: str, audio_name: str = 'output') -> str:
+    """F
+    Download a video from a url and return the path
+    """
+
+    # Remove audio file if it exists
+    audio_file_path = f'{audio_name}.wav'
+    if os.path.exists(audio_file_path):
+        os.remove(audio_file_path)
+    else:
+        print("The file does not exist")
+
+    try:
+        process = subprocess.run(_RETRIEVE_AUDIO_CMD_TEMPLATE.format(url=url.strip(),audio_name=audio_name), shell=True, check=True, text=True)
+        print(process.stdout)  # Imprime la salida estándar
+        print(process.stderr)  # Imprime el error estándar
+
+    except subprocess.CalledProcessError as e:
+        print(f'Error downloading video: {e.stderr}')
+        pass
+
+    print(f"Comand:{_RETRIEVE_AUDIO_CMD_TEMPLATE.format(url=url.strip(),audio_name=audio_name)}")
+
+    """
+    if status != 0:
+        raise Exception(f'Error downloading video: {stderr}')
+    
+    """
+
+    print('Downloaded video Filepath: ', f'{audio_name}.wav')
+    # return the path to the video
+
+    return f'{audio_name}.wav'
+
+
+# Function to split audio into vocal and accompaniment
+# pip install spleeter
+def split_audio(audio_file_path):
+
+    # Get the output directory
+    output_dir = os.path.dirname(audio_file_path)
+
+    # Extract file name with extension
+    audio_filename_ext = os.path.basename(audio_file_path)
+    audio_filename = os.path.splitext(audio_filename_ext)[0]
+
+    try:
+        # Execute the command
+        print(_SPLIT_AUDIO_CMD_TEMPLATE.format(audio_file_path=audio_file_path))
+        process = subprocess.run(_SPLIT_AUDIO_CMD_TEMPLATE.format(audio_file_path=audio_file_path), shell=True, check=True, text=True)
+        print(process.stdout)  # Imprime la salida estándar
+        print(process.stderr)  # Imprime el error estándar  
+    except subprocess.CalledProcessError as e:
+        print(f'Error splitting audio: {e.stdout}')
+        print(f'Error splitting audio: {e.stderr}')
+        pass
+    
+    # return the paths of the vocal and accompaniment files
+    return f'output/{audio_filename}/vocals.wav', f'output/{audio_filename}/accompaniment.wav'
+
+
+# Function to transcribe the vocals and return the lyrics
+def transcribe_vocals(audio_vocals_path):
+    # Devolver el resultado
+    model_size = "large-v3"
+
+    # Run on GPU with FP16
+    #model = WhisperModel(model_size, device="cuda", compute_type="float16")
+
+    # or run on GPU with INT8
+    # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
+    # or run on CPU with INT8
+    model = WhisperModel(model_size, device="cpu", compute_type="int8")
+
+    segments, info = model.transcribe(audio_vocals_path, beam_size=5)
+
+    print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+
+    for segment in segments:
+        print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+
+    return list(segments), info
+
+
+def run_cmd_sync(cmd, cwd=None, interactive=False, timeout=None):
+    """Runs a console command synchronously and returns the results.
+
+    Parameters
+    ----------
+    cmd : str
+       The command to execute.
+    cwd : :class:`pathlib.Path`, optional
+       The working directory in which to execute the command.
+    interactive : bool, optional
+       If set, run command interactively and pipe all output to console.
+    timeout : float, optional
+       If specified, kills process and throws error after this many seconds.
+
+    Returns
+    -------
+    int
+       Process exit status code.
+    str, optional
+       Standard output (if not in interactive mode).
+    str, optional
+       Standard error (if not in interactive mode).
+
+    Raises
+    ------
+    :class:`FileNotFoundError`
+       Unknown command.
+    :class:`NotADirectoryError`
+       Specified working directory is not a directory.
+    :class:`subprocess.TimeoutExpired`
+       Specified timeout expired.
+    """
+    if cmd is None or len(cmd.strip()) == 0:
+        raise FileNotFoundError()
+
+    kwargs = {}
+    if not interactive:
+        kwargs["stdout"] = subprocess.PIPE
+        kwargs["stderr"] = subprocess.PIPE
+
+    err = None
+    with subprocess.Popen(shlex.split(cmd), cwd=cwd, **kwargs) as p:
+        try:
+            p_res = p.communicate(timeout=timeout)
+        except subprocess.TimeoutExpired as e:
+            err = e
+        p.kill()
+
+    if err is not None:
+        raise err
+
+    result = p.returncode
+
+    if not interactive:
+        stdout, stderr = [s.decode("utf-8").strip() for s in p_res]
+        result = (result, stdout, stderr)
+
+    return result
+
 
 # vimeo url to embed url
 def convert_vimeo_url_to_embed(url: str) -> str:
